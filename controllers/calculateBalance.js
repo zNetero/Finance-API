ba72073835714a57572c411d3c978e89142907e4
Giordano <giordanob056@gmail.com>
2025-08-30 11:01:41 +0200

    Adjust calculateBalance.js to correctly compute balance

--- controllers/calculateBalance.js
+++ controllers/calculateBalance.js
@@ -2,7 +2,7 @@ const Transaction = require('../models/Transactions');
 
 async function calculateBalance(){
     try{
-        const balance = await Transaction.sum('value', where({type: 'income'})) - await Transaction.sum('value', where({type: 'expense'}));
+        const balance = await Transaction.sum('value', {where:{type: 'income'}}) - await Transaction.sum('value', {where:{type: 'expense'}});
         return balance
     }catch(error){
         throw error
